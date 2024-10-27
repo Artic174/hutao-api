477b191aed4581e9840e874815e7a8a1dfc20c36
Artic174 <142976550+Artic174@users.noreply.github.com>
2024-10-27 16:29:07 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ toggleSidebar, toggleTheme, theme }) => {
   return (
     <header className={styles.header}>
       <button onClick={toggleSidebar} className={styles.menuButton}>☰</button>
-      <div className={styles.logo}>Emilia's API</div>
+      <div className={styles.logo}>Hu Tao's API</div>
       <div className={styles.icons}>
         <a href="https://github.com/cifumo" target="_blank" rel="noopener noreferrer">
           <FaGithub className={styles.icon} />
